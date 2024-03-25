bd5cdad48a29a0c6f1b0f67ee96e8162a0b551fe
returnT0 <vlvolkov25@gmail.com>
2024-03-25 23:54:51 +0100

    modified pattern/condition services

--- src/app/shared/services/condition/condition.service.ts
+++ src/app/shared/services/condition/condition.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConditionService {
+  private baseUrl = 'http://localhost:3000/api';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  fetchAllConditions(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.baseUrl}/fetch-all-conditions`);
+  }
+
+  createCondition(text: string): Observable<any> {
+    return this.http.post(`${this.baseUrl}/create-condition`, { text });
+  }
+
+  editCondition(id: string, text: string): Observable<any> {
+    return this.http.put(`${this.baseUrl}/edit-condition/${id}`, { text });
+  }
 }
--- src/app/shared/services/pattern/pattern.service.ts
+++ src/app/shared/services/pattern/pattern.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PatternService {
+  private apiUrl = 'http://localhost:3000/api';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  fetchPattern(patternId: string): Observable<any> {
+    return this.http.get(`${this.apiUrl}/fetch-pattern/${patternId}`);
+  }
+
+  fetchAllPatterns(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/fetch-all-patterns`);
+  }
+
+  createPattern(name: string, conditionIds: string[]): Observable<any> {
+    return this.http.post(`${this.apiUrl}/create-pattern`, { name, conditionIds });
+  }
+
+  editPattern(id: string, newName: string, newConditions: string[]): Observable<any> {
+    return this.http.put(`${this.apiUrl}/edit-pattern/${id}`, { newName, newConditions });
+  }
+
+  deletePattern(patternId: string): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/delete-pattern/${patternId}`);
+  }
 }
